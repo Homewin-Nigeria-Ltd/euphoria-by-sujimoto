161146c0842e47299f784b027e471d5b12e00eed
Michael O. Ajayi <ajayimichael@hotmail.com>
2025-02-19 01:15:37 +0100

    Attempting to fix why the images isn't changing on prod but working fine on local

--- src/app/components/ImageCarousel.tsx
+++ src/app/components/ImageCarousel.tsx
@@ -6,6 +6,8 @@ import { useMediaQuery } from "react-responsive";
 
 const ImageCarousel = () => {
   const isMobile = useMediaQuery({ maxWidth: 767 });
+  const [mounted, setMounted] = useState(false);
+
   const images = [
     "/images/food/01.svg",
     "/images/food/02.svg",
@@ -16,38 +18,48 @@ const ImageCarousel = () => {
     "/images/food/07.svg",
   ];
   const [currentIndex, setCurrentIndex] = useState(0);
+  // const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      setCurrentIndex((prevIndex) => {
-        return prevIndex === images.length - 1 ? 0 : prevIndex + 1;
-      });
-    }, 500);
-
-    return () => clearInterval(interval);
+    setMounted(true);
   }, []);
 
+  useEffect(() => {
+    if (mounted) {
+      const interval = setInterval(() => {
+        setCurrentIndex((prevIndex) => {
+          return prevIndex === images.length - 1 ? 0 : prevIndex + 1;
+        });
+      }, 500);
+      return () => clearInterval(interval);
+    }
+  }, [mounted, images.length]);
+
+  if (!mounted) {
+    return null;
+  }
+
+  const dimensions = {
+    width: isMobile ? 400 : 426,
+    height: 294,
+  };
+
   return (
-    <div
-      className={`overflow-hidden ${isMobile ? "w-[400px]" : "w-[426px]"} h-[294px]`}
-    >
-      {isMobile ? (
+    <div className={`relative overflow-hidden w-full h-[294px] max-w-[426px]`}>
+      {images.map((src, index) => (
         <Image
-          src={images[currentIndex]}
-          alt={`Image ${currentIndex + 1}`}
-          className="w-full h-full object-cover transition-opacity duration-200"
-          width={400}
-          height={294}
+          key={src}
+          src={src}
+          alt={`Image ${index + 1}`}
+          className={`absolute top-0 left-0 w-full h-full object-cover transition-opacity duration-200 ${
+            currentIndex === index ? "opacity-100" : "opacity-0"
+          }`}
+          width={dimensions.width}
+          height={dimensions.height}
+          priority={index === 0}
+          // onLoadingComplete={() => setIsLoading(false)}
         />
-      ) : (
-        <Image
-          src={images[currentIndex]}
-          alt={`Image ${currentIndex + 1}`}
-          className="w-full h-full object-cover transition-opacity duration-200"
-          width={426}
-          height={294}
-        />
-      )}
+      ))}
     </div>
   );
 };
